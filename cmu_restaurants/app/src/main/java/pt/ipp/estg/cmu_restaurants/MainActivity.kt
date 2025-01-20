d7a1cbc9778e262a894cb6e194d356af868737ab
IanDCosta <8220005@estg.ipp.pt>
2025-01-20 19:24:30 +0000

    initial

---
+++ cmu_restaurants/app/src/main/java/pt/ipp/estg/cmu_restaurants/MainActivity.kt
@@ -0,0 +1,47 @@
+package pt.ipp.estg.cmu_restaurants
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import pt.ipp.estg.cmu_restaurants.ui.theme.Cmu_restaurantsTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Cmu_restaurantsTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Cmu_restaurantsTheme {
+        Greeting("Android")
+    }
+}
